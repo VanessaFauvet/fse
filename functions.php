7240bb08bd9430befdf18aeb488b011fccb8c897
VanessaF <89605309+VanessaFauvet@users.noreply.github.com>
2026-02-28 15:57:41 +0100

    Mise en place thème

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+# Retirer les accents des noms de fichiers
+add_filter('sanitize_file_name', 'remove_accents');
+
+# Déclarer les scripts et les styles
+function fse_register_assets()
+{
+    # Charger la feuille style.css du thème
+    wp_enqueue_style('main', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
+}
+add_action('wp_enqueue_scripts', 'fse_register_assets');
